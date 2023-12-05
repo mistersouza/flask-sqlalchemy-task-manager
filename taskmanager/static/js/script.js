0b6fc8badfc44ad70c939e2dfa85876ae2475ec2
Thiago Souza <mistersouza@icloud.com>
2023-12-05 13:18:30 +0000

    Add basic html structure and style header and footer

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // init burger menu
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
