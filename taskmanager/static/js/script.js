9f5cfba9c2f2d8b90e5fa95f8a63359ca58126da
Thiago Souza <mistersouza@icloud.com>
2023-12-12 17:48:47 +0000

    Read tasks table and style tasks page

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     // init burger menu
     let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav, options);
+    M.Sidenav.init(sidenav);
 
     // init date picker
     let datepicker = document.querySelectorAll('.datepicker');
     M.Datepicker.init(datepicker, {
-      format: 'dd mm, yy',
+      format: 'dd mmmm, yy',
       i18: {
         done: 'Select'
       }
@@ -15,4 +15,8 @@ document.addEventListener('DOMContentLoaded', function() {
     // select init
     let selects = document.querySelectorAll("select");
     M.FormSelect.init(selects);
+
+    // callapsible init
+    let collapsibles = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapsibles);
   });
