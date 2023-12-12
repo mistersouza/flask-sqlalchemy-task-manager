286d812dd3266aa5ebea2d4e50ad1605638748e2
Thiago Souza <mistersouza@icloud.com>
2023-12-12 11:43:27 +0000

    Rename variables

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
     // init burger menu
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav, options);
+
+    // init date picker
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: 'dd mm, yy',
+      i18: {
+        done: 'Select'
+      }
+    });
+
+    // select init
+    let selects = document.querySelectorAll("select");
+    M.FormSelect.init(selects);
   });
