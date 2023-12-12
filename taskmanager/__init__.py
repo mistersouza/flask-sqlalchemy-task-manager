50b1ace3f70c0acafb014c451816f2815a0d240e
Thiago Souza <mistersouza@icloud.com>
2023-12-12 20:40:09 +0000

    Set up for deployment

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -6,8 +6,13 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 # Set up the app configuration
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URL')
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URL')
+    if uri.startswith("postgres://"):
+         uri = uri.replace("postgres://", "postgresql://", 1)
+     app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
 db = SQLAlchemy(app)
 
